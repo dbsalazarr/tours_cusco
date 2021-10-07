cbdace64573404d8d69b50f7edd2832a7ad441d9
dbsalazarr <dbsalazarr@gmail.com>
2021-10-07 16:54:42 -0500

    Modificaciones a varios archivos

--- functions.php
+++ functions.php
@@ -7,18 +7,26 @@
 		// Añadiendo el soporte de titulos personalizados
 		add_theme_support("title-tag");
 
+		// Agregando detalles para un logo personalizado
+		$imagen = array(
+			'height' => 260,
+			'width' =>  280
+		)
+		add_theme_support('custom-logo', $imagen);
+
 		// Registrando nuevos tamaños de imagen
 
 
 	}
 	add_action("after_setup_theme", "tours_cusco_setup");
 
+
 	function tours_cusco_scripts(){
 
 		// Registrando los estilos CSS
 		wp_register_style("normalize", get_template_directory_uri().'/css/normalize.css', array(), '8.0.13');
 		wp_register_style("style", get_stylesheet_uri(), array('normalize'), '1.0', 'all');
-		wp_register_style('google-fonts', '', array('normalize', 'style'), '1.0');
+		wp_register_style('google-fonts', '"https://fonts.googleapis.com/css2?family=Jost:wght@400;700&family=Montserrat:wght@300;400;700&display=swap', array('normalize', 'style'), '1.0');
 
 		wp_enqueue_style("normalize");
 		wp_enqueue_style("style");
@@ -34,5 +42,16 @@
 		
 	}
 	add_action("wp_enqueue_scripts", "tours_cusco_scripts");
+
+
+	function tours_cusco_menus(){
+		register_nav_menus(array(
+			'menu_principal' => __('Menú principal', 'Machupicchu Tours Cusco'),
+			'menu_social' => __('Menú social', 'Machupicchu Tours Cusco Social'),
+			'menu_footer' => __('Menú footer', 'Machupicchu Tours Cusco Footer')
+		));
+
+	}
+	add_action("init", "tours_cusco_menus");
 
 ?>
--- header.php
+++ header.php
@@ -1,10 +1,26 @@
 <!DOCTYPE html>
-<html lang="es">
+<html <?php language_attributes(); ?>>
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-	
 	<?php wp_head(); ?>
 </head>
 <body>
-	<h3>Cabecera</h3>
+	<!-- MENÚ DE NAVEGACIÓN -->
+	<div class="content-nav-principal">
+		<div class="logo">
+			<?php 
+				if(function_exists('the_custom_logo')) :
+					the_custom_logo();
+				endif;
+			 ?>
+		</div>
+		<div class="menu-principal">
+			<!-- Definiendo el menú de navegación principal-->
+
+		</div>
+		<div class="menu-social">
+			<!-- Definiendo el menú de navegación social -->
+			
+		</div>
+	</div>
